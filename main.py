76b780697380d385674f736101607fc78cb42024
Wsteward504 <wsteward3rd@gmail.com>
2026-01-17 18:02:01 -0600

    adding all files two

---
+++ main.py
@@ -0,0 +1,8 @@
+# This program defines a function to greet a person and uses it.
+def greet(name):
+    print(f"Hello, {name}!")
+
+# Call the function for different names
+greet('Jack')
+greet('Jill')
+greet('Bob')
